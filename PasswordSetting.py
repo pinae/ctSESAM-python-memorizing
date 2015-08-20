bd04ee892dad66583036e92f7b6d613768636483
pinae <jonny@pinae.net>
2015-08-20 11:20:56 +0200

    Added docstrings

--- PasswordSetting.py
+++ PasswordSetting.py
@@ -3,7 +3,6 @@
 
 from datetime import datetime
 import string
-import json
 from base64 import b64encode, b64decode
 
 DEFAULT_SALT = "pepper".encode('utf-8')
@@ -14,6 +13,9 @@ DEFAULT_CHARACTER_SET_EXTRA = '#!"§$%&/()[]{}=-_+*<>;:.'
 
 
 class PasswordSetting(object):
+    """
+    This saves one set of settings for a certain domain. Use a PasswordSettingsManager to save the settings to a file.
+    """
     def __init__(self, domain):
         self.domain = domain
         self.username = None
@@ -28,37 +30,77 @@ class PasswordSetting(object):
         self.synced = False
 
     def get_domain(self):
+        """
+        Returns the domain name or another string used in the domain field.
+        :return:
+        """
         return self.domain
 
     def set_domain(self, domain):
+        """
+        Change the domain string.
+        :param domain:
+        :return:
+        """
         self.domain = domain
         self.synced = False
 
     def get_username(self):
+        """
+        Returns the username or an empty string if there was no username.
+        :return:
+        """
         if self.username:
             return self.username
         else:
             return ""
 
     def set_username(self, username):
+        """
+        Set the username.
+        :param username:
+        :return:
+        """
+        if username != self.username:
+            self.synced = False
         self.username = username
-        self.synced = False
 
     def get_legacy_password(self):
+        """
+        Returns the legacy password if set or an empty string otherwise.
+        :return:
+        """
         if self.legacy_password:
             return self.legacy_password
         else:
             return ""
 
     def set_legacy_password(self, legacy_password):
+        """
+        Set a legacy password.
+        :param legacy_password:
+        :return:
+        """
+        if legacy_password != self.legacy_password:
+            self.synced = False
         self.legacy_password = legacy_password
-        self.synced = False
 
     def use_letters(self):
+        """
+        Returns true if the character set contains the default set of letters at the default position and with the
+        default order.
+        :return:
+        """
         return self.used_characters[:len(DEFAULT_CHARACTER_SET_LOWER_CASE + DEFAULT_CHARACTER_SET_UPPER_CASE)] == \
             DEFAULT_CHARACTER_SET_LOWER_CASE + DEFAULT_CHARACTER_SET_UPPER_CASE
 
     def set_use_letters(self, use_letters):
+        """
+        If set to True the letters are moved to the default position and brought into the default order. Missing
+        letters are inserted. If set to False all default letters are removed from the character set.
+        :param use_letters:
+        :return:
+        """
         old_character_set = self.used_characters
         pos = 0
         while pos < len(self.used_characters):
@@ -73,9 +115,21 @@ class PasswordSetting(object):
             self.synced = False
 
     def use_lower_case(self):
+        """
+        Returns true if the character set contains the default set of lower case letters at the default position and
+        with the default order.
+        :return:
+        """
         return self.used_characters[:len(DEFAULT_CHARACTER_SET_LOWER_CASE)] == DEFAULT_CHARACTER_SET_LOWER_CASE
 
     def set_use_lower_case(self, use_lower_case):
+        """
+        If set to True the lower case letters are moved to the default position and brought into the default order.
+        Missing lower case letters are inserted. If set to False all lower case letters are removed from the character
+        set.
+        :param use_lower_case:
+        :return:
+        """
         old_character_set = self.used_characters
         pos = 0
         while pos < len(self.used_characters):
@@ -89,12 +143,24 @@ class PasswordSetting(object):
             self.synced = False
 
     def use_upper_case(self):
+        """
+        Returns true if the character set contains the default set of upper case letters at the default position and
+        with the default order.
+        :return:
+        """
         return self.used_characters[
             len(DEFAULT_CHARACTER_SET_LOWER_CASE):len(
                 DEFAULT_CHARACTER_SET_LOWER_CASE + DEFAULT_CHARACTER_SET_UPPER_CASE)] \
             == DEFAULT_CHARACTER_SET_UPPER_CASE
 
     def set_use_upper_case(self, use_upper_case):
+        """
+        If set to True the upper case letters are moved to the default position and brought into the default order.
+        Missing upper case letters from the default set of upper case letters are inserted. If set to False all
+        default upper case letters are removed from the character set.
+        :param use_upper_case:
+        :return:
+        """
         old_character_set = self.used_characters
         pos = 0
         while pos < len(self.used_characters):
@@ -109,12 +175,22 @@ class PasswordSetting(object):
             self.synced = False
 
     def use_digits(self):
+        """
+        Returns true if the character set contains digits at the default position and with the default order.
+        :return:
+        """
         return self.used_characters[
             len(DEFAULT_CHARACTER_SET_LOWER_CASE + DEFAULT_CHARACTER_SET_UPPER_CASE):len(
                 DEFAULT_CHARACTER_SET_LOWER_CASE + DEFAULT_CHARACTER_SET_UPPER_CASE + DEFAULT_CHARACTER_SET_DIGITS)] \
             == DEFAULT_CHARACTER_SET_DIGITS
 
     def set_use_digits(self, use_digits):
+        """
+        If set to True the digits are moved to the default position and brought into the default order.
+        Missing digits are inserted. If set to False all digits are removed from the character set.
+        :param use_digits:
+        :return:
+        """
         old_character_set = self.used_characters
         pos = 0
         while pos < len(self.used_characters):
@@ -131,11 +207,23 @@ class PasswordSetting(object):
             self.synced = False
 
     def use_extra(self):
+        """
+        Returns true if the character set contains the special characters from the default set at the default position
+        and with the default order.
+        :return:
+        """
         return self.used_characters[
             len(DEFAULT_CHARACTER_SET_LOWER_CASE + DEFAULT_CHARACTER_SET_UPPER_CASE + DEFAULT_CHARACTER_SET_DIGITS):] \
             == DEFAULT_CHARACTER_SET_EXTRA
 
     def set_use_extra(self, use_extra):
+        """
+        If set to True the default special characters are moved to the default position and brought into the default
+        order. Missing special characters from the default set are inserted. If set to False all special characters
+        from the default set are removed from the character set.
+        :param use_extra:
+        :return:
+        """
         old_character_set = self.used_characters
         pos = 0
         while pos < len(self.used_characters):
@@ -149,30 +237,63 @@ class PasswordSetting(object):
             self.synced = False
 
     def use_custom_character_set(self):
+        """
+        Returns true if the character set is set to the default character set.
+        :return:
+        """
         return not self.used_characters == DEFAULT_CHARACTER_SET_LOWER_CASE + DEFAULT_CHARACTER_SET_UPPER_CASE + \
             DEFAULT_CHARACTER_SET_DIGITS + DEFAULT_CHARACTER_SET_EXTRA
 
     @staticmethod
     def get_default_character_set():
+        """
+        Returns the default character set. This is completely independent of the character set stored at instances
+        of this class.
+        :return:
+        """
         return DEFAULT_CHARACTER_SET_LOWER_CASE + DEFAULT_CHARACTER_SET_UPPER_CASE + \
             DEFAULT_CHARACTER_SET_DIGITS + DEFAULT_CHARACTER_SET_EXTRA
 
     def get_character_set(self):
+        """
+        Returns the character set as a string.
+        :return:
+        """
         return self.used_characters
 
     def set_custom_character_set(self, character_set):
+        """
+        Sets the character set to the given string. Use this method to save reordered default sets.
+        :param character_set:
+        :return:
+        """
         if self.used_characters != character_set:
             self.synced = False
         self.used_characters = character_set
 
     def get_salt(self):
+        """
+        Returns the salt.
+        :return:
+        """
         return self.salt
 
     @staticmethod
     def get_default_salt():
+        """
+        This returns the default salt. This is completely independent of the salt stored at instances
+        of this class.
+        :return:
+        """
         return DEFAULT_SALT
 
     def set_salt(self, salt):
+        """
+        You should normally pass bytes as a salt. For convenience this method also accepts strings which get
+        UTF-8 encoded and stored in binary format. If in doubt pass bytes.
+        :param salt:
+        :return:
+        """
         if type(salt) == bytes:
             if self.salt != salt:
                 self.synced = False
@@ -185,28 +306,59 @@ class PasswordSetting(object):
             raise TypeError("The salt should be bytes.")
 
     def get_length(self):
+        """
+        Returns the desired password length.
+        :return:
+        """
         return self.length
 
     def set_length(self, length):
+        """
+        Sets the desired length.
+        :param length:
+        :return:
+        """
         if self.length != length:
             self.synced = False
         self.length = length
 
     def get_iterations(self):
+        """
+        Returns the iteration count which is to be used.
+        :return:
+        """
         return self.iterations
 
     def set_iterations(self, iterations):
+        """
+        Sets the iteration count integer.
+        :param iterations:
+        :return:
+        """
         if self.iterations != iterations:
             self.synced = False
         self.iterations = iterations
 
     def get_c_date(self):
+        """
+        Returns the creation date as a datetime object.
+        :return:
+        """
         return self.creation_date
 
     def get_creation_date(self):
+        """
+        Returns the creation date as string.
+        :return:
+        """
         return self.creation_date.strftime("%Y-%m-%dT%H:%M:%S")
 
     def set_creation_date(self, creation_date):
+        """
+        Sets the creation date passed as string.
+        :param creation_date:
+        :return:
+        """
         if self.creation_date != creation_date:
             self.synced = False
         try:
@@ -218,12 +370,25 @@ class PasswordSetting(object):
             self.modification_date = self.creation_date
 
     def get_m_date(self):
+        """
+        Returns the modification date as a datetime object.
+        :return:
+        """
         return self.modification_date
 
     def get_modification_date(self):
+        """
+        Returns the modification date as string.
+        :return:
+        """
         return self.modification_date.strftime("%Y-%m-%dT%H:%M:%S")
 
     def set_modification_date(self, modification_date=None):
+        """
+        Sets the modification date passed as string.
+        :param modification_date:
+        :return:
+        """
         if modification_date and self.modification_date != modification_date:
             self.synced = False
         if type(modification_date) == str:
@@ -240,21 +405,45 @@ class PasswordSetting(object):
             self.creation_date = self.modification_date
 
     def get_notes(self):
+        """
+        Returns the notes.
+        :return:
+        """
         if self.notes:
             return self.notes
         else:
             return ""
 
     def set_notes(self, notes):
+        """
+        Sets some note. This overwrites existing notes.
+        :param notes:
+        :return:
+        """
+        if notes != self.notes:
+            self.synced = False
         self.notes = notes
 
     def is_synced(self):
+        """
+        Query if the synced flag is set. The flag switches to false if settings are changed.
+        :return:
+        """
         return self.synced
 
     def set_synced(self, is_synced=True):
+        """
+        Sets the synced state. Call this after syncing.
+        :param is_synced:
+        :return:
+        """
         self.synced = is_synced
 
     def to_dict(self):
+        """
+        Returns a dictionary with settings to be saved.
+        :return:
+        """
         domain_object = {"domain": self.get_domain()}
         if self.get_username():
             domain_object["username"] = self.get_username()
@@ -272,6 +461,11 @@ class PasswordSetting(object):
         return domain_object
 
     def load_from_dict(self, loaded_setting):
+        """
+        Loads the setting from a dictionary.
+        :param loaded_setting:
+        :return:
+        """
         if "username" in loaded_setting:
             self.set_username(loaded_setting["username"])
         if "legacyPassword" in loaded_setting:
