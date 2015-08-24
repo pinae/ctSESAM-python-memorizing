6da3116278c3d27551b15f281f6c7361de6dec8a
pinae <jonny@pinae.net>
2015-08-24 16:02:12 +0200

    Added a domain extractor function

---
+++ domainExtractor.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import re
+
+
+def extract_top_domain(url):
+    """
+    Extracts the domain from an url. Subdomains are ignored
+
+    :param url: Url with https:// and /some/path
+    :type url: str
+    :return: domain name without protocol, subdomains or path
+    :rtype: str
+    """
+    pattern = re.compile("(?:https?://)?(\w+\.)+(co\.\w+).*")
+    matches = pattern.match(url)
+    if matches and len(matches.groups()) >= 2:
+        return matches.group(len(matches.groups()) - 1) + matches.group(len(matches.groups()))
+    pattern = re.compile("(?:https?://)?(\w+\.)+(\w+).*")
+    matches = pattern.match(url)
+    if matches and len(matches.groups()) >= 2:
+        return matches.group(len(matches.groups()) - 1) + matches.group(len(matches.groups()))
+    else:
+        return url
+
+
+def extract_full_domain(url):
+    """
+    Extracts the domain from an url
+
+    :param url: Url with https:// and /some/path
+    :type url: str
+    :return: domain name without protocol or path
+    :rtype: str
+    """
+    pattern = re.compile("(?:https?://)?((?:\w+\.)*)(\w+).*")
+    matches = pattern.match(url)
+    if matches and len(matches.groups()) >= 2:
+        return "".join(matches.groups())
+    else:
+        return url
---
+++ tests/test_domainExtractor.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import unittest
+from domainExtractor import extract_top_domain, extract_full_domain
+
+
+class TestDomainExtractor(unittest.TestCase):
+    def test_extract_top_domain(self):
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("http://www.test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("http://test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("http://complicated.subdomain.structure.test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("https://www.test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("https://test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("https://complicated.subdomain.structure.test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("test.com"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("www.test.com"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("complicated.subdomain.structure.test.com"))
+        self.assertEqual(
+            "test.com",
+            extract_top_domain("test.com/path/to/things"))
+        self.assertEqual(
+            "amazon.co.jp",
+            extract_top_domain("www.amazon.co.jp/search=?some(characters)[strange]"))
+        self.assertEqual(
+            "english.co.uk",
+            extract_top_domain("english.co.uk"))
+        self.assertEqual(
+            "noUrl",
+            extract_top_domain("noUrl"))
+
+    def test_extract_full_domain(self):
+        self.assertEqual(
+            "www.test.com",
+            extract_full_domain("http://www.test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_full_domain("http://test.com/some/path/index.html"))
+        self.assertEqual(
+            "complicated.subdomain.structure.test.com",
+            extract_full_domain("http://complicated.subdomain.structure.test.com/some/path/index.html"))
+        self.assertEqual(
+            "www.test.com",
+            extract_full_domain("https://www.test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_full_domain("https://test.com/some/path/index.html"))
+        self.assertEqual(
+            "complicated.subdomain.structure.test.com",
+            extract_full_domain("https://complicated.subdomain.structure.test.com/some/path/index.html"))
+        self.assertEqual(
+            "test.com",
+            extract_full_domain("test.com"))
+        self.assertEqual(
+            "www.test.com",
+            extract_full_domain("www.test.com"))
+        self.assertEqual(
+            "complicated.subdomain.structure.test.com",
+            extract_full_domain("complicated.subdomain.structure.test.com"))
+        self.assertEqual(
+            "test.com",
+            extract_full_domain("test.com/path/to/things"))
+        self.assertEqual(
+            "www.amazon.co.jp",
+            extract_full_domain("www.amazon.co.jp/search=?some(characters)[strange]"))
+        self.assertEqual(
+            "english.co.uk",
+            extract_full_domain("english.co.uk"))
+        self.assertEqual(
+            "noUrl",
+            extract_full_domain("noUrl"))
+
+
+if __name__ == '__main__':
+    unittest.main()
