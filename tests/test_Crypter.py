269c70bac0c457f46dd7168b97eec8ec254c93fb
pinae <jonny@pinae.net>
2015-08-25 16:00:36 +0200

    Repaired test: iteration count was increased

--- tests/test_Crypter.py
+++ tests/test_Crypter.py
@@ -9,27 +9,25 @@ from base64 import b64encode, b64decode
 class TestCrypter(unittest.TestCase):
     def test_encrypt(self):
         message_string = "Important information with quite some length. " + \
-                        "This message is as long as this because otherwise only one cipher block would " + \
-                        "be encrypted. This long message insures that more than one block is needed."
+                         "This message is as long as this because otherwise only one cipher block would " + \
+                         "be encrypted. This long message insures that more than one block is needed."
         password = "secret"
         message = message_string.encode('utf-8')
-        crypter = Crypter(password)
+        crypter = Crypter("pepper".encode('utf-8'), password)
         ciphertext = crypter.encrypt(message)
         self.assertEqual(
-            b'FRQFCWa38eSIrPnhELojAPrOb8oKzs2yoAbNqVONBEuac3OhUKY12mP+TNyZs1MRUbY9hnqvIG18' +
-            b'7MqTAVTzI0fCJhmR4stc/k4YpS+HptmzcTgEfXeli56davPUkmJ59yz2vvF3t/pCUOk0qWNQ2vv9' +
-            b'dU2sJhvOdQ7RVKzbw2DJAFtEM2BxJq8Oqa4mB4sBC/GpIP3xtNxANJPyN8xTSL2F4Ktt5hIcX3AV' +
-            b'UrnGYSjGeDHGua8iKNFohYtaPj3vvzaSVpGyzAfmlVEdN5/8zQ==',
+            b'1lsDkebMaZZeO+/DnvVAUYPmXrQOdCAFQ79C3sElpwamOLtX444tRMiecg4/a9394w51dbmKo89CYKpw19nOaKkbF8Dy' +
+            b'll9MQJSRUXZEoc3aoaBvgGBCy4rVM62hEQLfpOUdBcrJTPAU3l8zM8V+AN560z7Rj9gXoGkXsotIpEjNg0+hwdmcVRAw' +
+            b'JAiDnAbH7K1Q0olPdkM187tbF5A9OEzCU5M36qzUyr/68a1oGL65JCaMAGoHTpQa2i4DlTEkVF1xPkB40ZF167jo360lEQ==',
             b64encode(ciphertext))
 
     def test_decrypt(self):
-        cyphertext = "FRQFCWa38eSIrPnhELojAPrOb8oKzs2yoAbNqVONBEuac3OhUKY12mP+TNyZs1MRUbY9hnqvIG18" + \
-                     "7MqTAVTzI0fCJhmR4stc/k4YpS+HptmzcTgEfXeli56davPUkmJ59yz2vvF3t/pCUOk0qWNQ2vv9" + \
-                     "dU2sJhvOdQ7RVKzbw2DJAFtEM2BxJq8Oqa4mB4sBC/GpIP3xtNxANJPyN8xTSL2F4Ktt5hIcX3AV" + \
-                     "UrnGYSjGeDHGua8iKNFohYtaPj3vvzaSVpGyzAfmlVEdN5/8zQ=="
+        cyphertext = b'1lsDkebMaZZeO+/DnvVAUYPmXrQOdCAFQ79C3sElpwamOLtX444tRMiecg4/a9394w51dbmKo89CYKpw19nOaKkbF8Dy' + \
+            b'll9MQJSRUXZEoc3aoaBvgGBCy4rVM62hEQLfpOUdBcrJTPAU3l8zM8V+AN560z7Rj9gXoGkXsotIpEjNg0+hwdmcVRAw' + \
+            b'JAiDnAbH7K1Q0olPdkM187tbF5A9OEzCU5M36qzUyr/68a1oGL65JCaMAGoHTpQa2i4DlTEkVF1xPkB40ZF167jo360lEQ=='
         self.assertEqual(0, len(b64decode(cyphertext)) % 16)
         password = "secret"
-        crypter = Crypter(password)
+        crypter = Crypter("pepper".encode('utf-8'), password)
         self.assertEqual(b'Important information with quite some length. ' +
                          b'This message is as long as this because otherwise only one cipher block would ' +
                          b'be encrypted. This long message insures that more than one block is needed.',
