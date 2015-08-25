512d5442deb429ddc90eee5d8d7b6e95146233f0
pinae <jonny@pinae.net>
2015-08-25 16:04:47 +0200

    typo

--- tests/test_Crypter.py
+++ tests/test_Crypter.py
@@ -22,16 +22,16 @@ class TestCrypter(unittest.TestCase):
             b64encode(ciphertext))
 
     def test_decrypt(self):
-        cyphertext = b'1lsDkebMaZZeO+/DnvVAUYPmXrQOdCAFQ79C3sElpwamOLtX444tRMiecg4/a9394w51dbmKo89CYKpw19nOaKkbF8Dy' + \
+        ciphertext = b'1lsDkebMaZZeO+/DnvVAUYPmXrQOdCAFQ79C3sElpwamOLtX444tRMiecg4/a9394w51dbmKo89CYKpw19nOaKkbF8Dy' + \
             b'll9MQJSRUXZEoc3aoaBvgGBCy4rVM62hEQLfpOUdBcrJTPAU3l8zM8V+AN560z7Rj9gXoGkXsotIpEjNg0+hwdmcVRAw' + \
             b'JAiDnAbH7K1Q0olPdkM187tbF5A9OEzCU5M36qzUyr/68a1oGL65JCaMAGoHTpQa2i4DlTEkVF1xPkB40ZF167jo360lEQ=='
-        self.assertEqual(0, len(b64decode(cyphertext)) % 16)
+        self.assertEqual(0, len(b64decode(ciphertext)) % 16)
         password = "secret"
         crypter = Crypter("pepper".encode('utf-8'), password)
         self.assertEqual(b'Important information with quite some length. ' +
                          b'This message is as long as this because otherwise only one cipher block would ' +
                          b'be encrypted. This long message insures that more than one block is needed.',
-                         crypter.decrypt(b64decode(cyphertext)))
+                         crypter.decrypt(b64decode(ciphertext)))
 
 if __name__ == '__main__':
     unittest.main()
